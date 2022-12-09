f4a803faa3d53aa0ec9c3b408158e1192dc68e47
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2022-12-09 19:07:30 +0000

    Cancel a marriage request after divorcing. (major)

--- commands/Social/divorce.js
+++ commands/Social/divorce.js
@@ -13,13 +13,17 @@ module.exports = {
         }
         else {
             await set(`${user.id}_marriageStatus`, 'single');
-            await set(`${user.id}_marriedTo`, null);
             await set(`${user.id}_marriageTime`, null);
+            await set(`${user.id}_marriageRequest`, null)
+            await set(`${user.id}_sender`, null);
             await set(`${spouse}_marriageStatus`, 'single');
             await set(`${spouse}_marriedTo`, null);
             await set(`${spouse}_marriageTime`, null);
+            await set(`${spouse}_sender`, null);
+            await set(`${spouse}_marriageRequest`, null)
+            await set(`${user.id}_marriedTo`, null);
             await incr(`${interaction.user.id}`, 'commandsUsed', 1);
-            return interaction.reply({ content: 'You are now divorced from <@' + spouse + '>!',});
+            return interaction.reply({ content: 'You are now divorced from <@' + spouse + '>.',});
         }
     },
 };
