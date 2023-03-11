2e8b82c740a41b542d727d66f5b91d7ec9b87e6d
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-03-11 23:51:12 +0000

    Fix sell command

--- commands/Shop/sell.js
+++ commands/Shop/sell.js
@@ -5,7 +5,7 @@ const {
   ButtonStyle,
   Events,
 } = require("discord.js");
-const { get, set, decr, coinEmoji } = require("../../globals.js");
+const { get, incr, decr, coinEmoji } = require("../../globals.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -52,28 +52,30 @@ module.exports = {
 
     // If the item doesn't exist, return
     if (!selectedItem) {
-        return interaction.reply({
-            content: `The item \`${item}\` doesn't exist.`,
-            ephemeral: true,
-        });
+      return interaction.reply({
+        content: `The item \`${item}\` doesn't exist.`,
+        ephemeral: true,
+      });
     }
 
     // If the user doesn't have the item, return
-    if (get(user.id, `${selectedItem.name}.amount`) < amount) {
-        return interaction.reply({
-            content: `You don't have enough \`${item}\` to sell.`,
-            ephemeral: true,
-        });
+    if ((await get(`${user.id}_${selectedItem.name}`)) < amount) {
+      return interaction.reply({
+        content: `You don't have enough \`${item}\` to sell.`,
+        ephemeral: true,
+      });
     }
 
     // If the user has the item, sell it
-    if (await get(user.id, `${selectedItem.name}.amount`) >= amount) {
-        await decr(user.id, `${selectedItem.name}`, amount);
-        await set(user.id, "coins", await get(user.id, "coins") + selectedItem.value * amount);
-        return interaction.reply({
-            content: `You sold \`${amount}\` \`${selectedItem.name}\` for ${coinEmoji}${selectedItem.value * amount}.`,
-            ephemeral: true,
-        });
+    if ((await get(`${user.id}_${selectedItem.name}`)) >= amount) {
+      await decr(user.id, `${selectedItem.name}`, amount);
+      await incr(user.id, "coins", selectedItem.value * amount)
+      return interaction.reply({
+        content: `You sold \`${amount}\` \`${
+          selectedItem.name
+        }\` for ${coinEmoji}${selectedItem.value * amount}.`,
+        ephemeral: true,
+      });
     }
   },
 };
