b0aacc8b492a1f424055e7aa33cabf6ac09c0d67
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-05-01 16:10:01 +0100

    /userinfo: "Commands used" no longer shows for bots

--- commands/Stats/userinfo.js
+++ commands/Stats/userinfo.js
@@ -17,18 +17,22 @@ module.exports = {
     const createdAt = Date.parse(user.createdAt) / 1000;
     const joinedAt =
       Date.parse(interaction.guild.members.cache.get(user.id).joinedAt) / 1000;
-    const roles = interaction.guild.members.cache.get(user.id).roles.cache.map(role => role.toString()).join(" ");
-    const numRoles = interaction.guild.members.cache.get(user.id).roles.cache.size;
+    const roles = interaction.guild.members.cache
+      .get(user.id)
+      .roles.cache.map((role) => role.toString())
+      .join(" ");
+    const numRoles = interaction.guild.members.cache.get(user.id).roles.cache
+      .size;
     if ((await get(`${user.id}_commandsUsed`)) === null) {
       await incr(`${user.id}`, "commandsUsed", 0);
     }
     const Embed = new EmbedBuilder()
       .setDescription(
-        `\`${user.username}#${user.discriminator}(${
-          user.id
-        })\`\n**Commands used:** \`${await get(
-          `${user.id}_commandsUsed`
-        )}\`\n**Joined Discord:** <t:${createdAt}>\n**Joined server:** <t:${joinedAt}>\n\n**Roles (${numRoles}):** ${roles}\n\n**Links:**${
+        `\`${user.username}#${user.discriminator}(${user.id})\`\n${
+          user.bot
+            ? ""
+            : `**Commands used:** \`${await get(`${user.id}_commandsUsed`)}\`\n`
+        }**Joined Discord:** <t:${createdAt}>\n**Joined server:** <t:${joinedAt}>\n\n**Roles (${numRoles}):** ${roles}\n\n**Links:**${
           user.avatarURL() ? `\n[Avatar](${user.avatarURL()})` : ""
         }${user.banner ? `\n[Banner](${fuser.bannerURL()})` : ""}`
       )
