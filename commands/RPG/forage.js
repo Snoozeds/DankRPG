346dc06ab185c09fe3f04b9d2fa8fde214eb62e2
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-05-03 20:52:29 +0100

    Fixed forage erroring with null items!

--- commands/RPG/forage.js
+++ commands/RPG/forage.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { get, incr, diamondEmoji } = require("../../globals.js");
+const { get, set, incr, diamondEmoji } = require("../../globals.js");
 const { CommandCooldown, msToMinutes } = require("discord-command-cooldown");
 const ms = require("ms");
 const chance = require("chance").Chance();
@@ -11,6 +11,7 @@ module.exports = {
     .setName("forage")
     .setDescription("Forages for items in the wilderness."),
   async execute(interaction) {
+    const user = interaction.user;
     const started = await get(`${interaction.user.id}_hasStarted`);
     if (started === undefined) {
       await interaction.reply({
@@ -19,7 +20,18 @@ module.exports = {
       });
     }
 
-    const userCooldowned = await forageCommandCooldown.getUser(interaction.user.id);
+    // The command errors if the user's items are not defined (null.)
+    const items = ["stone", "wood", "diamond"];
+
+    for (const item of items) {
+      if (!(await get(`${user.id}_${item}`))) {
+        await set(`${user.id}_${item}`, 0);
+      }
+    }
+
+    const userCooldowned = await forageCommandCooldown.getUser(
+      interaction.user.id
+    );
     if (userCooldowned) {
       const timeLeft = msToMinutes(userCooldowned.msLeft, false);
       await interaction.reply({
@@ -28,7 +40,6 @@ module.exports = {
       });
     } else {
       const rare = chance.weighted([true, false], [10, 90]); // 10% chance of getting a rare item
-      const user = interaction.user;
       const embed = new EmbedBuilder()
         .setTitle("Foraging...")
         .setDescription(`<@${user.id}> goes foraging in the wilderness.`)
@@ -42,7 +53,7 @@ module.exports = {
         await incr(user.id, "diamond", 1);
       } else {
         const amount = chance.integer({ min: 3, max: 5 });
-        const set = chance.weighted(["wood", "stone"], [40, 60])
+        const set = chance.weighted(["wood", "stone"], [40, 60]);
         embed.setFields({
           name: `${set}`,
           value: `You found ${amount} **${set}**!`,
