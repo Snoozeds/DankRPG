195dd70ea4c8274df5203e8fb67d83be8a235ce7
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-07-18 09:12:17 +0100

    Fix /avatar not working on GIFs + add embed.

--- commands/Images/avatar.js
+++ commands/Images/avatar.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
-const { incr } = require("../../globals.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { get } = require("../../globals.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,9 +8,16 @@ module.exports = {
     .addUserOption((option) => option.setName("user").setDescription("The member who's avatar you want to view").setRequired(false)),
   async execute(interaction) {
     const user = interaction.options.getUser("user") ?? interaction.user;
-    const attachment = new AttachmentBuilder(user.displayAvatarURL({ dynamic: true, size: 4096 })).setName("avatar.png");
-    await interaction.reply({
-      files: [attachment],
-    });
+    const embed = new EmbedBuilder()
+      .setTitle(`${user.username}'s avatar`)
+      .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+      .setDescription(
+        `Download sizes:\n[512x512](${user.displayAvatarURL({ dynamic: true, size: 512 })}) | [1024x1024](${user.displayAvatarURL({
+          dynamic: true,
+          size: 1024,
+        })}) | [2048x2048](${user.displayAvatarURL({ dynamic: true, size: 2048 })}) | [4096x4096](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`
+      )
+      .setColor(await get(`${user.id}_color`));
+    await interaction.reply({ embeds: [embed] });
   },
 };
