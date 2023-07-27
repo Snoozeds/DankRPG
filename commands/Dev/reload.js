06d8ceed5051c2ed6935f1b346d9cce01ca36f6c
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-07-27 13:21:58 +0100

    New: /reload - reload all commands without restart

---
+++ commands/Dev/reload.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("node:fs");
+
+module.exports = {
+  nonGlobal: true,
+  data: new SlashCommandBuilder()
+    .setName("reload")
+    .setDescription("Reloads a command.")
+    .addStringOption((option) => option.setName("command").setDescription("The command you want to reload.").setRequired(true)),
+  async execute(interaction) {
+    if (interaction.user.id !== "764564962815115267") return interaction.reply({ content: "You are not authorized to use this command.", ephemeral: true });
+
+    const commandName = interaction.options.getString("command");
+    const command = interaction.client.commands.get(commandName);
+    let commandLocation = "";
+
+    if (!command) return interaction.reply({ content: `There is no command with the name \`${commandName}\`.`, ephemeral: true });
+
+    // Loop through the ./commands folder and its subfolders to find the command specified.
+    const commandFolders = fs.readdirSync("./commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith(".js"));
+      for (const file of commandFiles) {
+        // If the command is found, delete it from the cache.
+        if (file === `${commandName}.js`) {
+          delete require.cache[require.resolve(`../${folder}/${file}`)];
+          commandLocation = `../${folder}/${file}`;
+        }
+      }
+    }
+
+    try {
+      interaction.client.commands.delete(command.data.name);
+      const newCommand = require(commandLocation);
+      interaction.client.commands.set(newCommand.data.name, newCommand);
+      await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply(`There was an error while reloading the command \`${command.data.name}\`:\n\`${error.message}\``);
+    }
+  },
+};
