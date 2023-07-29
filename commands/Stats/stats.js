27dddbdde81ecd5c63996b2780df16bedde4a6ee
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-07-29 19:13:18 +0100

    /skills renamed to /stats, improve fields.

--- commands/Stats/stats.js
+++ commands/Stats/stats.js
@@ -2,19 +2,19 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { get, hpEmoji, armorEmoji, attackEmoji, levelEmoji } = require("../../globals.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("skills").setDescription("Explains your skills."),
+  data: new SlashCommandBuilder().setName("stats").setDescription("View your stats."),
   async execute(interaction) {
     const embed = new EmbedBuilder()
       .setTitle("Skills")
       .addFields(
         {
           name: `${hpEmoji} MaxHP (${await get(`${interaction.user.id}_max_hp`)})`,
-          value: `The maximum amount of HP you can have.`,
+          value: `The maximum amount of health points you can have.`,
           inline: false,
         },
         {
           name: `${hpEmoji} HP (${await get(`${interaction.user.id}_hp`)})`,
-          value: `Your current HP.`,
+          value: `Your current health points.`,
           inline: false,
         },
         {
@@ -24,12 +24,12 @@ module.exports = {
         },
         {
           name: `${attackEmoji} Damage (${await get(`${interaction.user.id}_damage`)})`,
-          value: `Increases your damage dealt in fights.`,
+          value: `Increases the damage you deal in fights.`,
           inline: false,
         },
         {
           name: `${levelEmoji} Level (${await get(`${interaction.user.id}_level`)})`,
-          value: `You gain more rewards per level. You gain XP by using commands.`,
+          value: `Your stats increase as you level up. Level up by using RPG commands.`,
           inline: false,
         }
       )
