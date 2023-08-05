cc5b0108c8858bd5920c14bdc599492948e34c5c
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-08-05 16:36:14 +0100

    /serverinfo: No longer show "Links" when none.

--- commands/Stats/serverinfo.js
+++ commands/Stats/serverinfo.js
@@ -5,16 +5,28 @@ module.exports = {
   data: new SlashCommandBuilder().setName("serverinfo").setDescription("Get info about the current server."),
   async execute(interaction) {
     const server = interaction.guild;
+    const serverIcon = server.iconURL();
+    const serverBanner = server.bannerURL();
+
     const embed = new EmbedBuilder()
       .setDescription(
         `\`${server.name} (${server.id})\`\n**Owner:** ${await server.fetchOwner()}\n**Created:** <t:${Date.parse(server.createdAt) / 1000}> (<t:${
           Date.parse(server.createdAt) / 1000
         }:R>)\n**Members:** ${server.memberCount}\n**Emojis:** ${server.emojis.cache.size}\n**Boosts:** ${server.premiumSubscriptionCount}\n**Boost Level:** ${
           server.premiumTier
-        }\n\n**Links:**${server.iconURL() ? `\n[Icon](${server.iconURL()})` : ""}${server.bannerURL() ? `\n[Banner](${server.bannerURL()})` : ""}`
+        }`
       )
       .setColor(await get(`${interaction.user.id}_color`))
-      .setThumbnail(server.iconURL());
+      .setThumbnail(server.iconURL())
+      .setImage(server.bannerURL());
+
+    if (serverIcon || serverBanner) {
+      let links = "\n\n**Links:**";
+      if (serverIcon) links += `\n[Icon](${serverIcon})`;
+      if (serverBanner) links += `\n[Banner](${serverBanner})`;
+      embed.setDescription(embed.description + links);
+    }
+
     await interaction.reply({ embeds: [embed] });
   },
 };
