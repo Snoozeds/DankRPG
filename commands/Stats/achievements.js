2e5a0883324374065816752fcba38db4d170eb99
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-08-20 16:26:43 +0100

    Update emoji in /achievements

--- commands/Stats/achievements.js
+++ commands/Stats/achievements.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { get, set, incr, perc, falseEmoji, trueEmoji, coinEmoji } = require("../../globals.js");
+const { get, set, perc, emoji } = require("../../globals.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -72,25 +72,25 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle(`${user.username}'s Achievements`)
       .setDescription(
-        `**__It Begins...__** ${dailyAchievement == "true" ? trueEmoji : falseEmoji}
+        `**__It Begins...__** ${dailyAchievement == "true" ? emoji.achievementUnlock : emoji.achievementLock}
 Get your first daily reward.
-Reward: ${coinEmoji}250
+Reward: ${emoji.coins}250
 
-**__Learner__** ${learnerAchievement == "true" ? trueEmoji : falseEmoji}
+**__Learner__** ${learnerAchievement == "true" ? emoji.achievementUnlock : emoji.achievementLock}
 View \`/commands\` for the first time.
-Reward: ${coinEmoji}100
+Reward: ${emoji.coins}100
 
-**__Feared__** ${fearedAchievement == "true" ? trueEmoji : falseEmoji}
+**__Feared__** ${fearedAchievement == "true" ? emoji.achievementUnlock : emoji.achievementLock}
 Win 100 fights.
-Reward: ${coinEmoji}1000
+Reward: ${emoji.coins}1000
 
-**__Dedicated__** ${dedicatedAchievement == "true" ? trueEmoji : falseEmoji}
+**__Dedicated__** ${dedicatedAchievement == "true" ? emoji.achievementUnlock : emoji.achievementLock}
 Collect 30 daily rewards in a row without missing a day.
-Reward: ${coinEmoji}500
+Reward: ${emoji.coins}500
 
-**__April Fools!__** ${aprilAchievement == "true" ? trueEmoji : falseEmoji}
+**__April Fools!__** ${aprilAchievement == "true" ? emoji.achievementUnlock : emoji.achievementLock}
 Use any command between 1st-3rd April.
-Reward: ${coinEmoji}500`
+Reward: ${emoji.coins}500`
       )
       .setThumbnail(user.displayAvatarURL({ dynamic: true, size: 4096 }))
       .setFooter({
