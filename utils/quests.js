d24b8168dca3237affd8aa8b62b2aaba4d168574
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-08-26 14:44:50 +0100

    remove testChron()

--- utils/quests.js
+++ utils/quests.js
@@ -79,32 +79,15 @@ cron.schedule("0 0 * * *", async () => {
     cursor = nextCursor;
   } while (cursor !== "0");
 
-  // Pick 3 random quests, ensure the same quest isn't picked more than once
-  const randomQuests = [];
-  while (randomQuests.length < 3) {
-    const randomQuest = quests[Math.floor(Math.random() * Object.keys(quests).length) + 1];
-    if (!randomQuests.includes(randomQuest)) {
-      randomQuests.push(randomQuest);
-    }
-  }
-
-  await redis.set("quests", JSON.stringify(randomQuests)); // Add the quests to the key
-  console.log("Added new quests: ", randomQuests);
-});
-
-async function testChron() {
-  console.log("Starting quests cron job...");
-  await redis.del("quests"); // Remove all quests.
-
-  // Fetch all user keys that match the pattern "*_questsCompleted"
-  let cursor = "0";
+  // Fetch all user keys that match the pattern "*_treesChopped" (used for quest 5)
+  cursor = "0";
   do {
-    const [nextCursor, keys] = await redis.scan(cursor, "MATCH", "*_questsCompleted");
+    const [nextCursor, keys] = await redis.scan(cursor, "MATCH", "*_treesChopped");
     if (keys.length > 0) {
       // Iterate over the found keys and delete them
       for (const key of keys) {
         await redis.del(key);
-        console.log(`Deleted questsCompleted key: ${key}`);
+        console.log(`Deleted treesChopped key: ${key}`);
       }
     }
     cursor = nextCursor;
@@ -121,8 +104,4 @@ async function testChron() {
 
   await redis.set("quests", JSON.stringify(randomQuests)); // Add the quests to the key
   console.log("Added new quests: ", randomQuests);
-  process.exit(); // Exit the process
-}
-
-// Run from cmd line to test.
-testChron();
+});
