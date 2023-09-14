59c43c8a39ee9f05d60c24c4519c8707ffb75d3c
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-09-14 17:40:58 +0100

    /avatar will now display server-specific avatars.

--- commands/Images/avatar.js
+++ commands/Images/avatar.js
@@ -8,16 +8,41 @@ module.exports = {
     .addUserOption((option) => option.setName("user").setDescription("The member who's avatar you want to view").setRequired(false)),
   async execute(interaction) {
     const user = interaction.options.getUser("user") ?? interaction.user;
-    const embed = new EmbedBuilder()
-      .setTitle(`${user.username}'s avatar`)
-      .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
-      .setDescription(
-        `Download sizes:\n[512x512](${user.displayAvatarURL({ dynamic: true, size: 512 })}) | [1024x1024](${user.displayAvatarURL({
-          dynamic: true,
-          size: 1024,
-        })}) | [2048x2048](${user.displayAvatarURL({ dynamic: true, size: 2048 })}) | [4096x4096](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`
-      )
-      .setColor((await get(`${interaction.user.id}_color`)) ?? "#2b2d31");
-    await interaction.reply({ embeds: [embed] });
+    const userAvatar = user.displayAvatarURL({ dynamic: true, size: 4096 });
+    const member = interaction.guild.members.cache.get(user.id) ?? interaction.member;
+    const memberAvatar = member.displayAvatarURL({ dynamic: true, size: 4096 });
+
+    if (memberAvatar === userAvatar) {
+      const embed = new EmbedBuilder()
+        .setTitle(`${user.username}'s avatar`)
+        .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+        .setDescription(
+          `Download sizes:\n[512x512](${user.displayAvatarURL({ dynamic: true, size: 512 })}) | [1024x1024](${user.displayAvatarURL({
+            dynamic: true,
+            size: 1024,
+          })}) | [2048x2048](${user.displayAvatarURL({ dynamic: true, size: 2048 })}) | [4096x4096](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`
+        )
+        .setColor((await get(`${interaction.user.id}_color`)) ?? "#2b2d31");
+      await interaction.reply({ embeds: [embed] });
+    } else {
+      const embed = new EmbedBuilder()
+        .setTitle(`${user.username}'s avatar`)
+        .setThumbnail(member.displayAvatarURL({ dynamic: true, size: 4096 }))
+        .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+        .setDescription(
+          `Server avatar:\n[512x512](${member.displayAvatarURL({ dynamic: true, size: 512 })}) | [1024x1024](${member.displayAvatarURL({
+            dynamic: true,
+            size: 1024,
+          })})\n[2048x2048](${member.displayAvatarURL({ dynamic: true, size: 2048 })}) | [4096x4096](${member.displayAvatarURL({
+            dynamic: true,
+            size: 4096,
+          })})\n\nUser avatar:\n[512x512](${user.displayAvatarURL({ dynamic: true, size: 512 })}) | [1024x1024](${user.displayAvatarURL({
+            dynamic: true,
+            size: 1024,
+          })})\n[2048x2048](${user.displayAvatarURL({ dynamic: true, size: 2048 })}) | [4096x4096](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`
+        )
+        .setColor((await get(`${interaction.user.id}_color`)) ?? "#2b2d31");
+      await interaction.reply({ embeds: [embed] });
+    }
   },
 };
